172584abc17eb580f52b093538f5ef1648fcce68
KarunakarThurlu <karunakarthurlujava@gmail.com>
2024-06-29 19:17:12 +0530

    Adding CORS Config

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import path from 'path'
+import cors from 'cors'
 
 const app = express();
 const PORT = process.env.PORT || 2024
@@ -8,6 +9,27 @@ const PORT = process.env.PORT || 2024
 app.use("/info", (req, res) => {
     res.send('Hello, World!');
 })
+
+// Define your allowed origins
+const allowedOrigins = [
+    'https://reactapp-m3tu.onrender.com'
+];
+
+// Set up CORS middleware
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin)
+            callback(null, true);
+        else
+            callback(new Error('Not allowed by CORS'));
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Specify allowed methods
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'], // Specify allowed headers
+    credentials: true // Allow credentials if needed
+};
+
+app.use(cors(corsOptions));
+
 
 app.use(express.static(path.join(process.cwd(), './client/dist')));
 app.get('/*', (req, res) => {
